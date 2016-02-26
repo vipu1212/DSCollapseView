2b71199fa7b8f75099451629516252d821d52f3a
Divyansh Singh <divyansh.1212@gmail.com>
2016-02-26 13:18:39 +0530

    Initial commit

---
+++ Example/DSCollapseView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DSCollapseView
+//
+//  Created by Divyansh Singh on 02/26/2016.
+//  Copyright (c) 2016 Divyansh Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
